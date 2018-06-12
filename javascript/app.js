65f54ec07038d8a4201c804c4bffcf3579a72b1f
antoinehirtz <antoine.hirtz@telecom-paristech.fr>
2018-06-12 22:42:06 +0200

    Update for full datasets loading

--- javascript/app.js
+++ javascript/app.js
@@ -28,6 +28,8 @@ const colorMap = [
 
 // Define time formats
 
+const datetimeFormat = d3.timeFormat('%d %B %H:%m');
+const monthFormat = d3.timeFormat('%B %Y');
 const dayFormat = d3.timeFormat('%d');
 const hourFormat = d3.timeFormat('%H');
 
@@ -53,7 +55,10 @@ function renderColor() {
         .domain([0, 1])
         .range(colorMap);
 
-  rect.attr("fill", (d) => quantizeScale(d.index));
+  rect.transition()
+    //.delay((d) => dayFormat(d.datetime) * 15)
+    .duration(transition_duration)
+    .attr("fill", (d) => quantizeScale(d.index));
 
 }
 
@@ -61,9 +66,9 @@ function renderColor() {
 
 function draw() {
 
-  var setIds = d3.map(dataset, (d) => d.name).keys();
+  var setOfName = d3.map(dataset, (d) => d.name).keys();
 
-  var setDatetime = d3.map(dataset, (d) => dayFormat(d.datetime)).keys();
+  var setOfDatetime = d3.map(dataset, (d) => dayFormat(d.datetime)).keys();
 
   // Initialize svg element
 
@@ -87,7 +92,7 @@ function draw() {
     .attr("width", itemSize)
     .attr("height", cellSize)
     .attr("x", (d) => itemSize * (dayFormat(d.datetime) - 1))
-    .attr("y", (d) => itemSize * (setIds.indexOf(d.name)))
+    .attr("y", (d) => itemSize * (setOfName.indexOf(d.name)))
     .attr("fill", d3.color("white"))
     .on('mouseover', function(d) {
       // Pulsating animation
@@ -96,7 +101,8 @@ function draw() {
       d3.select("#tooltip")
         .style("left", d3.event.pageX + "px")
         .style("top", d3.event.pageY + "px")
-        .select("#value").text("index : " + d.index + "\nname : " + d.name + "\ndate : " + dayFormat(d.datetime));
+        .select("#value")
+        .text("index : " + d.index + "\ndate : " + datetimeFormat(d.datetime) + "\nname : " + d.name);
       d3.select("#tooltip")
         .classed("hidden", false);
     })
@@ -109,6 +115,14 @@ function draw() {
 
   renderColor();
 
+  // Plot the "timestamp"
+
+  svg.append("text")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+    .attr("y", 0 - (margin.top / 2))
+    .attr("class", "timestamp month")
+    .text(monthFormat(dataset[0].datetime));
+
   // Plot the "x axis"
 
   svg.append("g").selectAll('.label-datetime')
@@ -118,19 +132,24 @@ function draw() {
     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
     .attr("class", "label label-datetime")
     .text(function (d, i) {
-      return i < setDatetime.length ? setDatetime[i] : '';
+      return i < setOfDatetime.length ? setOfDatetime[i] : '';
     })
-    .attr("x", (d, i) => ((i % setDatetime.length) + 0.3) * itemSize)
+    .attr("x", (d, i) => (i + 0.3) * itemSize)
     .attr("y", -15)
     .on('mouseenter', function(d, i) {
-      var selected_datetime = setDatetime[i];
+      var selected_datetime = setOfDatetime[i];
 
-      console.log("mouse on : ", setDatetime[i]);
+      // for debugging :
+      console.log("mouse on : ", setOfDatetime[i]);
       
       heatmap.selectAll("rect")
         .transition()
         .duration(transition_duration)
         .style('opacity', function(d) {
+
+          // for debugging :
+          console.log('selected_datetime : ' + selected_datetime + ' / d.datetime : ' + dayFormat(d.datetime));
+
           return dayFormat(d.datetime) == selected_datetime ? 1 : 0;
         });
     })
@@ -147,23 +166,28 @@ function draw() {
     .data(dataset)
     .enter()
     .append("text")
-    .attr("transform", "translate(" + (margin.left - 40) + ", " + margin.top + ")")
-    .attr("class", "label label-row")
     .text(function (d, i) {
-      return i < setIds.length ? setIds[i] : '';
+      return i < setOfName.length ? setOfName[i] : '';
     })
-    .style("font-weight", "bold")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+    .attr("x", (d) => - 10 * d.name.length)
     .attr("y", (d, i) => (i + 0.5) * itemSize)
+    .attr("class", "label label-row")
+    .style("font-weight", "bold")
     .on('mouseenter', function(d, i) {
-      var selected_name = setIds[i];
+      var selected_name = setOfName[i];
 
+      // for debugging :
       console.log("mouse on : ", selected_name);
       
       heatmap.selectAll("rect")
         .transition()
         .duration(transition_duration)
         .style('opacity', function(d) {
-          console.log('selected_name : ' + selected_name + '/ d.name : ' + d.name);
+
+          // for debugging :
+          console.log('selected_name : ' + selected_name + ' / d.name : ' + d.name);
+
           return d.name == selected_name ? 1 : 0;
         });
     })
