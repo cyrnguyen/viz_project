b84cecc2dece34a5a2c7ae5204882902c08184b2
antoinehirtz <antoine.hirtz@telecom-paristech.fr>
2018-06-17 23:16:14 +0200

    + datetime selection

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,58 +1,102 @@
-var json = "parcs.json" 
-
+// Declare canvas parameters
 const margin = { top: 80, bottom: 80, left: 100, right: 0 };
-const width = 1500;
+const width = 3000;
 const height = 1000;
 const label_padding = 40;
-const item_size = 30;
+const item_size = 34;
 const cell_size = item_size - 4;
 
+// Declare time duration for d3.transition
 const transition_duration = 500; // ms
 
-const getDateTime = d3.timeFormat('%d %B %H:%m');
-const getMonth = d3.timeFormat('%B %Y');
+// Declare d3.time formats
+const getDate = d3.timeFormat('%Y%m%d');
+const getYear = d3.timeFormat('%Y');
+const getMonth = d3.timeFormat('%B');
 const getDay = d3.timeFormat('%d');
 const getHour = d3.timeFormat('%H');
 
+// Declare color map
 const color_map = [
-  "#9E0142", // strong red
-  "#FDAE61", // orange
-  "#F6FAAA", // yellow
-  "#8BE0AE", // light green
-  "#4CA64C", // green
-  "#007300", // strong green
+  '#9E0142', // strong red
+  '#FDAE61', // orange
+  '#F6FAAA', // yellow
+  '#8BE0AE', // light green
+  '#4CA64C', // green
+  '#007300', // strong green
 ];
 
-var svg = d3.select("[role='heatmap']");
+// Define scaled color map
+const quantizeScale = d3.scaleQuantize()
+  .domain([0, 1]) // health index in interval [0, 1] 
+  .range(color_map);
 
+// Define record features
+let json = 'parcs.json' 
+let overview = 'month';
+let history = ['month'];
+let selected = {};
+let data = {};
+
+// Instantiate SVG element
+var svg = d3.select("[role='heatmap']");
+// Instantiate "heatmap calendar" element
 var heatmap = svg.attrs({width : width, height : height})
-  .append("g")
+  .append('g')
   .attrs({width: width - margin.left - margin.right,
           height: height - margin.top - margin.bottom})
-  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+  .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+// Instantiate "back button" element
+var button = svg.append('g');
+// Instantiate "names axis" element
+var names_axis = svg.append('g');
+// Instantiate "time axis" element
+var time_axis = svg.append('g');
+// Instantiate "timestamp" element
+var timestamp = svg.append('g');
 
-var names_axis = svg.append("g")
 
-var button = svg.append("g")
-
-
+//Define draw "back button" function
 function drawButton() {
-
   button.attr('class', 'button')
     .style('opacity', 0)
     .on('click', function () {
-      json = "parcs.json"
-      loadParks(json);
+      // calendar : filtered on specified datetime value(s)
+      if (Object.keys(selected)[0] == 'datetime') {
+        if (!('parc' in selected)) {
+          delete selected.datetime;
+          history.pop();
+          overview = history[history.length - 1];
+          data_loaded();
+        } else {
+          delete selected.parc;
+          json = 'parcs.json';
+          loadParks(json);
+        }
+      }
+      // calendar : filtered on a specified parc
+      else if (Object.keys(selected)[0] == 'parc') {
+        if (!('datetime' in selected)) {
+          delete selected.parc;
+          json = 'parcs.json';
+          loadParks(json);
+        } else {
+          delete selected.datetime;
+          history.pop();
+          overview = history[history.length - 1];
+          data_loaded();
+        }
+      }
     });
 
-  button.append("circle")
+  button.append('circle')
     .attr('cx', label_padding)
     .attr('cy', label_padding)
     .attr('r', item_size / 2);
 
-  button.append("text")
+  button.append('text')
     .attr('x', label_padding)
-    .attr('y', label_padding - 1)
+    .attr('y', label_padding - 6)
     .attr('dy', function () {
       return Math.floor(width / 100) / 2.5;
     })
@@ -62,19 +106,20 @@ function drawButton() {
     .html('&#x2190;');
 
   button.transition()
-    .duration(transition_duration)
+    //.duration(transition_duration)
     .style('opacity', 1)
-    .style('display', function(d) { return json == "parcs.json" ? "none" : "flex"; })
+    .style('display', function(d) {
+      return (json == 'parcs.json') && (overview == 'month') && (!Object.keys(selected).length) ? 'none' : 'flex';
+    })
 
 }
-
 
 // Define nesting function
 function indexGroupBy(time_unit) {
 
-  if (time_unit === "day")
+  if (overview == 'month')
     f = getDay;
-  else if (time_unit === "hour")
+  else if (overview == 'day')
     f = getHour;
 
   var nested_index = d3.nest()
@@ -83,7 +128,7 @@ function indexGroupBy(time_unit) {
     .rollup(function(values) {
       return d3.min(values, function(d) {return d.index; })
     })
-    .map(dataset);
+    .map(data);
 
   var nested_index_to_array = [];
 
@@ -99,53 +144,49 @@ function indexGroupBy(time_unit) {
 
 }
 
-
-// Define color function
-function printColor() {
-
-  var quantizeScale = d3.scaleQuantize()
-        .domain([0, 1])
-        .range(color_map);
-
-  rect.transition()
-    .delay((d) => d.datetime * 15)
-    .duration(transition_duration)
-    .attr("fill", function (d) {
-      idx = d.index
-
-      if (idx > 1) {
-        // whitesmoke color for missing values
-        return "#F5F5F5";
-      }
-
-      return quantizeScale(idx);
-    });
-
-}
-
-
 function draw() {
 
-  var set_names = d3.map(dataset, (d) => d.name).keys();
+  // Add current overview to the history
+  if (history[history.length - 1] !== overview) {
+    history.push(overview);
+  }
 
-  var set_datetimes = d3.map(dataset, (d) => getDay(d.datetime)).keys();
+  if ('datetime' in selected) {
+    data = dataset.filter((e) => +getDay(e.datetime) == selected.datetime);
+  } else {
+    data = dataset;
+  }
 
-  var daily_index = indexGroupBy("day");
+  var nested_index;
+  if (overview == 'month')
+    nested_index = indexGroupBy('day');
+  else if (overview == 'day')
+    nested_index = indexGroupBy('hour');
 
-  rect = heatmap.selectAll("rect")
-    .data(daily_index)
+  var set_names = d3.map(nested_index, (d) => d.name).keys();
+
+  var set_datetimes = d3.map(nested_index, (d) => d.datetime).keys()
+    .sort(function (x, y) { return d3.ascending(+x, +y); })
+
+  rect = heatmap.selectAll('rect')
+    .data(nested_index)
     .enter()
-    .append("rect")
+    .append('rect')
     .attrs({width: item_size, height: cell_size})
-    .attr("x", (d) => item_size * (d.datetime - 1))
-    .attr("y", (d) => item_size * (set_names.indexOf(d.name)))
-    .attr("fill", d3.color("white"))
-    .on('mouseover', function(d) {
+    .attr('x', (d) => overview == 'month' ? item_size * (d.datetime - 1) : item_size * d.datetime)
+    .attr('y', (d) => item_size * (set_names.indexOf(d.name)))
+    .attr('fill', d3.color('white'))
+    .on('mouseover', function(d, i) {
       d3.select("#tooltip")
-        .style("left", d3.event.pageX + "px")
-        .style("top", d3.event.pageY + "px")
+        .style('left', d3.event.pageX + 'px')
+        .style('top', d3.event.pageY + 'px')
         .select("#value")
-        .text(d.name + "\non the " + d.datetime + " " + getMonth(dataset[0].datetime) + "\nindex : " + d.index.toFixed(3));
+        .text(function () {
+          if (overview == 'month')
+            return d.name + ', on ' + getMonth(data[0].datetime) + ' ' + d.datetime + '\nindex : ' + d.index.toFixed(3)
+          else if (overview == 'day')
+            return d.name + ', on ' + getMonth(data[0].datetime) + ' ' + selected.datetime + ' at ' + d.datetime + ':00\nindex : ' + d.index.toFixed(3)
+        });
       d3.select("#tooltip")
         .classed("hidden", false);
     })
@@ -154,76 +195,111 @@ function draw() {
         .classed("hidden", true);
     });
 
-  printColor();
+  // Print colors
+  rect.transition()
+    //.delay((d) => d.datetime * 15)
+    //.duration(transition_duration)
+    .attr('fill', function (d) {
+      idx = d.index
+      // whitesmoke color for missing values (index = 2 if NA)
+      if (idx > 1) { return '#F5F5F5'; }
+      return quantizeScale(idx);
+    });
+
+  // Plot the "color-map description"
+  d3.select('[role="calibration"] [role="example"]')
+    .select('svg').selectAll('rect')
+    .data(color_map)
+    .enter()
+    .append('rect')
+    .attr('width', cell_size - 10)
+    .attr('height', cell_size - 10)
+    .attr('x', function (d, i) { return i * (cell_size - 9); })
+    .attr('fill', (d) => d);
 
   // Plot the "timestamp"
-  svg.append("text")
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-    .attr("y", 0 - (margin.top / 2))
-    .attr("class", "timestamp month")
-    .text(getMonth(dataset[0].datetime));
+  var month_timestamp = getMonth(dataset[0].datetime) + ' ' + getYear(dataset[0].datetime);
+  var day_timestamp = getMonth(dataset[0].datetime) + ' ' + selected.datetime + ', ' + getYear(dataset[0].datetime);
+  timestamp.select('text').remove();
+  timestamp.append('text')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    .attr('y', 0 - (margin.top / 2))
+    .attr('class', "timestamp month")
+    .text(overview == 'month' ? month_timestamp : day_timestamp);
 
   // Plot the datetime axis
-  svg.append("g").selectAll('.label-datetime')
-    .data(dataset)
+  time_axis.selectAll(".label-datetime").remove();
+  time_axis.selectAll(".label-datetime")
+    .data(data)
     .enter()
-    .append("text")
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-    .attr("class", "label label-datetime")
+    .append('text')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    .attr('class', "label label-datetime")
     .text(function (d, i) {
-      return i < set_datetimes.length ? set_datetimes[i] : '';
+      if (overview == 'month')
+        return set_datetimes[i];
+      else if (overview == 'day')
+        return i < set_datetimes.length ? set_datetimes[i] + ':00' : '';
     })
-    .attr("x", (d, i) => (i + 0.3) * item_size)
-    .attr("y", -15)
+    .style('font-size', (overview == 'month') ? '12px' : '10px')
+    .attr('x', (d, i) => (overview == 'month') ? (i + 0.3) * item_size : (i + 0.2) * item_size)
+    .attr('y', -12)
     .on('mouseenter', function(d, i) {
-      var selected_datetime = set_datetimes[i];
-      heatmap.selectAll("rect")
+      var pointed_datetime = set_datetimes[i];
+      heatmap.selectAll('rect')
         .transition()
-        .duration(transition_duration)
+        //.duration(transition_duration)
         .style('opacity', function(d) {
-          return d.datetime == selected_datetime ? 1 : 0.1;
+          return d.datetime == pointed_datetime ? 1 : 0.1;
         });
     })
     .on('mouseout', function() {
-      heatmap.selectAll("rect")
+      heatmap.selectAll('rect')
         .transition()
-        .duration(transition_duration)
-        .style('opacity', 1);
-    })
-
-  // Plot the names axis
-
-  names_axis.selectAll('.label-row').remove();
-  names_axis.selectAll('.label-row')
-    .data(dataset)
-    .enter()
-    .append("text")
-    .text(function (d, i) {
-      return i < set_names.length ? set_names[i] : '';
-    })
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-    .attr("x", (d) => - 10 * d.name.length)
-    .attr("y", (d, i) => (i + 0.5) * item_size)
-    .attr("class", "label label-row")
-    .style("font-weight", "bold")
-    .on('mouseenter', function(d, i) {
-      var selected_name = set_names[i];
-      heatmap.selectAll("rect")
-        .transition()
-        .duration(transition_duration)
-        .style('opacity', function(d) {
-          return d.name == selected_name ? 1 : 0.1;
-        });
-    })
-    .on('mouseout', function() {
-      heatmap.selectAll("rect")
-        .transition()
-        .duration(transition_duration)
+        //.duration(transition_duration)
         .style('opacity', 1);
     })
     .on('click', function(d, i) {
-      var selected_parc = set_names[i];
-      json = selected_parc + ".json"
+      if (overview == 'month') {
+        selected['datetime'] = set_datetimes[i];
+        overview = 'day';
+        data_loaded();
+      }
+    })
+
+  // Plot the names axis
+  names_axis.selectAll(".label-row").remove();
+  names_axis.selectAll(".label-row")
+    .data(data)
+    .enter()
+    .append('text')
+    .text(function (d, i) {
+      return set_names[i];
+    })
+    .style('font-size', '14px')
+    .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    .attr('x', (d) => - 10 * d.name.length)
+    .attr('y', (d, i) => (i + 0.5) * item_size)
+    .attr('class', "label label-row")
+    .style('font-weight', 'bold')
+    .on('mouseenter', function(d, i) {
+      var pointed_name = set_names[i];
+      heatmap.selectAll('rect')
+        .transition()
+        //.duration(transition_duration)
+        .style('opacity', function(d) {
+          return d.name == pointed_name ? 1 : 0.1;
+        });
+    })
+    .on('mouseout', function() {
+      heatmap.selectAll('rect')
+        .transition()
+        //.duration(transition_duration)
+        .style('opacity', 1);
+    })
+    .on('click', function(d, i) {
+      selected['parc'] = set_names[i];
+      json = selected['parc'] + '.json';
       loadParks(json);
     })
 
@@ -231,11 +307,9 @@ function draw() {
 
 }
 
-
 // The function run when the data is loaded
 function data_loaded() {
-    heatmap.selectAll("rect").remove();
-    console.log('json : ' + json);
+    heatmap.selectAll('rect').remove();
     draw();
 }
 
